15a9ddee9878444820b216c1c8ca943c5fa59c12
Ashish Kumar <118570586+ashish-vsy@users.noreply.github.com>
2024-12-19 03:29:29 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,20 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
+export const connectDB = async () => {
+  try {
+    // Adding the database name after the '/' in the URI (replace 'yourDatabase' with the actual DB name you want to connect to)
+    const uri = 'mongodb+srv://ashishkrobo:ashish123@cluster0.sgksj.mongodb.net/yourDatabase?retryWrites=true&w=majority&appName=Cluster0';
 
-export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://ashishkrobo:ashish123@cluster0.sgksj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB connected"));
-}
+    // Connecting to MongoDB using Mongoose
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // SSL should be enabled by default with mongodb+srv, but you can explicitly specify it if needed
+      ssl: true
+    });
+
+    console.log("DB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
