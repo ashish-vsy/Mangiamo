88cafc91dc2f27f05d2cfdb2204c20d5f48cc2fd
Ashish Kumar <118570586+ashish-vsy@users.noreply.github.com>
2024-12-20 00:36:27 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
 
-const url = "https://mangiamo-backend.onrender.com/";
+const url = "https://mangiamo-backend.onrender.com";
 
   return (
     <div>
